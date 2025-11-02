58522a8b99599d59432935a0eb2ef8d0723e9ee9
Mohan Raja Ram R K <mohanram2812@gmail.com>
2025-11-02 16:06:47 +0530

    Refactored component structure and fixed import issues

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -5,7 +5,8 @@ import "./index.css";
 const Header = () => {
   return (
     <header className="header">
-      <h1 className="logo">ShopEase</h1>
+      <h1 className="logo">Discover Our Products</h1>
+      <p className="para">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
       <input type="text" placeholder="Search products..." className="search-bar" />
     </header>
   );
